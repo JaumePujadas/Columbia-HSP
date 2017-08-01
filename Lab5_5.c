99c5056dcb0e4c14b5d959fc9751f07765a1f3f3
JaumePujadas <jcp132242@gmail.com>
2017-08-01 13:01:45 -0400

    Update Lab5_5.c

--- Lab5_5.c
+++ Lab5_5.c
@@ -9,42 +9,32 @@
 #include <stdio.h>
 #include <string.h>
 int x;
-char *scanline(char *dest, int dest_len);
 int main()
 {
+	struct data
+	{
+		char fname[20];
+		char lname[20];
+		int age;
+	};
 	printf("How many names would you like to put in?");
 	scanf("%d", &x);
-	char str[x][100];
-	char fname[20], lname[20];
-	int ages[x];
+	struct data d[x];
 	for(int i=0;i<x;i++)
 	{
 		printf("Enter name #%d(Lastname, Firstname):\n", (i+1));
-		scanf("%s %s", &lname, &fname);
+		scanf("%s %s", &d[i].lname, &d[i].fname);
 		printf("How old is this person?\n");
-		scanf("%d", &ages[i]);
-		strcat(str[i], lname);
-		printf("\n%d, %c\n", strlen(str[i]), str[i][strlen(str)]);
-		str[i][strlen(str)]= ' ';
-		strcat(str[i], fname);
-		puts(str[i]);
+		scanf("%d", &d[i].age);
 	}
-}
-char *scanline(char *dest, /* output - destination string */ int dest_len) /* input - space available in dest */
-{
-  int i, ch;
-
-  /* Gets next line one character at a time. */
-  i = 0;
-  for (ch = getchar();
-       ch != '\n' && ch != EOF && i < dest_len - 1;
-       ch = getchar())
-    dest[i++] = ch;
-  dest[i] = '\0';
-
-  /* Discards any characters that remain on input line */
-  while (ch != '\n' && ch != EOF)
-    ch = getchar();
-
-  return dest;
+	for(int i=65;i<=90;i++)
+	{
+		for(int j=0;j<x;j++)
+		{
+			if(d[j].lname[0]==i)
+			{
+				printf("%s %s %d\n", d[j].lname, d[j].fname, d[j].age);
+			}
+		}
+	}
 }

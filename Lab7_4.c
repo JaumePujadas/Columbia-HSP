546530d067fb26012a94b1c823b2a198b019a3cb
JaumePujadas <jcp132242@gmail.com>
2017-08-03 12:01:07 -0400

    Add files via upload

--- Lab7_4.c
+++ Lab7_4.c
@@ -8,46 +8,51 @@
 */
 #include <stdio.h>
 #include <string.h>
-struct list
+struct list_info
 {
 	char lang[13];
 	int list_len;
 	char words[][20];
-}l1;
-int x;
-char loadList();
-char addWords();
-int contains(char word[]);
+};
+int x, len;
+char loadList(struct list_info l);
+char addWords(struct list_info l);
+int contains(char word[], struct list_info l);
 int main()
 {
-	char c;
-	int lnum1, lnum2;
-	loadList();
-	addWords();
+	printf("How many word lists would you like to input?\n");
+	scanf("%d", &len);
+	struct list_info l[len];
+	for(int i=0;i<len;i++)
+	{
+		loadList(l[i]);
+		addWords(l[i]);
+	}
 }
-char loadList()
+char loadList(struct list_info l)
 {
 	printf("What language are the words you would like to input in?\n");
-	scanf("%s", &l1.lang);
+	scanf("%s", &l.lang);
 	printf("How many words would you like to put in?\n");
 	scanf("%d", &x);
-	printf("Please enter %d words in %s\n", x, l1.lang);
+	printf("Please enter %d words in %s\n", x, l.lang);
 }
-char addWords()
+char addWords(struct list_info l)
 {
 	char temp[100];
-	for(l1.list_len=0;l1.list_len<x;l1.list_len++)
+	for(l.list_len=0;l.list_len<x;l.list_len++)
 	{
 		scanf("%s", &temp);
 		for(int i=0;i<=100;i++)
 		{
 			if(temp[i]=='\0'&&i<=21)
 			{
-				strcpy(l1.words[l1.list_len], temp);
+				strcpy(l.words[l.list_len], temp);
 				i=101;
-				if(contains(l1.words[l1.list_len])==1)
+				printf("%s <--\n", l.words[l.list_len]);
+				if(contains(l.words[l.list_len], l)==1)
 				{
-					l1.list_len--;
+					l.list_len--;
 				}
 			}
 			else if (i>21)
@@ -59,13 +64,13 @@ char addWords()
 		}
 	}
 }
-int contains(char word[])
+int contains(char word[], struct list_info l)
 {
 	char c, ignore;
-	for(int i=0;i<l1.list_len;i++)
+	for(int i=0;i<l.list_len;i++)
 	{
-
-		if(strcmp(l1.words[i], word)==0)
+		printf("-->%s\n", l.words[0]);
+		if(strcmp(l.words[i], word)==0)
 		{
 			printf("Are you sure that you would like to add a duplicate word? [Y/N]\n");
 			do
